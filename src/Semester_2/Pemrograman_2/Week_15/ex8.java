95ec5e164b86bec5dea5664da2a3ffcc10ded1cf
Haqidhiya <haqidhiya04@gmail.com>
2024-06-03 08:22:03 +0700

    Upload & Update File

--- src/Semester_2/Pemrograman_2/Week_15/ex8.java
+++ src/Semester_2/Pemrograman_2/Week_15/ex8.java
@@ -4,6 +4,30 @@ package Semester_2.Pemrograman_2.Week_15;
 // NIM   : 23051130016
 // Kelas : J2
 
-public class ex8 {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class ex8 {
+    public static long fourLetterWords(String fileName) throws IOException {
+        return Files.lines(Paths.get(fileName))
+                .filter(line -> line.length() == 4)
+                .distinct()
+                .count();
+    }
+
+    public static void main(String[] args) {
+        try {
+            String fileName = "src/Semester_2/Pemrograman_2/Week_15/file.txt";
+            long fourLetterWordsCount = fourLetterWords(fileName);
+            System.out.println(fourLetterWordsCount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+/*
+ * Output
+ * 5
+ */
